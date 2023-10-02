be7e3c1bbc04663d1eabdba61f0d9dc0c3622ca2
Shreeya Rathod <shreeya.rathod1@gmail.com>
2023-10-02 17:33:50 +0100

    Creates check_guess function.

--- milestone_3.py
+++ milestone_3.py
@@ -21,3 +21,22 @@ if secret_word.find(guess) >= 0:
 else:
     print(f'Sorry, {guess} is not in the word. Try again.')
 # %%
+# check_guess function
+def check_guess(guess):
+    '''This function will take the guessed letter as an argument and check if the letter is in the word.'''
+   
+    # Convert guess to lowercase
+    guess_lowercase = guess.lower()
+    print(guess_lowercase)
+
+
+    secret_word.find(guess)
+
+    if secret_word.find(guess) >= 0:
+        print(f'Good guess! {guess} is in the word.')
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again.')
+
+# %%
+check_guess(guess)
+# %%

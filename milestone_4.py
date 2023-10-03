34ab8dacc46ced6556cd18750b1aaefec24c2f1a
Shreeya Rathod <shreeya.rathod1@gmail.com>
2023-10-03 14:37:25 +0100

    Initialises Hangman class

---
+++ milestone_4.py
@@ -0,0 +1,21 @@
+#%%
+# Import random module into script
+import random
+
+#%%
+# Create Hangman class
+
+class Hangman:
+    '''
+    This class
+
+    Attributes:
+        
+    '''
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+
+    
+
+    

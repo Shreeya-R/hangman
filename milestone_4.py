b904b20be4484227ce4a2b96f694855645e0024e
Shreeya Rathod <shreeya.rathod1@gmail.com>
2023-10-04 14:58:28 +0100

    Modifies create_word_guessed to add letters once guessed

--- milestone_4.py
+++ milestone_4.py
@@ -23,7 +23,7 @@ class Hangman:
         self.num_letters = num_letters
         self.list_of_guesses = list_of_guesses 
 
-    def check_guess(guess, word):
+    def check_guess(self, guess, word):
         '''
         This function takes the guessed letter as an argument and checks if the letter is in the secret word.
         '''
@@ -39,7 +39,7 @@ class Hangman:
         else:
             print(f'Sorry, {guess_lowercase} is not in the word. Try again.')
 
-    def ask_for_input(word):
+    def ask_for_input(self, word):
         '''
         This function validates whether or not thes input is a single alphabetical character.
         Also, uses the check_guess(guess) function to validate if the guessed letter is in the secret word.
@@ -55,6 +55,7 @@ class Hangman:
 
         return check_guess(guess, word)
     
+    #%%
     # word_guessed
     # A list of the word with _ for each letter not yet guessed.
     def create_word_guessed():
@@ -62,6 +63,7 @@ class Hangman:
         This function
         '''
         word_guessed = [ ]
+        word_lowercase = word.lower()
 
         for letter in word:
             word_guessed == word_guessed.append('_')
@@ -73,15 +75,18 @@ class Hangman:
         while True: 
             # guess = input('Please enter a single letter here: ')
 
-            ask_for_input(word)
+            guess = input('Please enter a single letter here: ')
+            guess_lowercase = guess.lower()
 
-            for letter in word:
-                if guess_lowercase == letter:
-                    letter_position = word.index(guess_lowercase)
-                    print(letter_position)
-                    word_guessed[letter_position] = guess_lowercase
-                else: 
-                    print(f'{guess_lowercase} is not in the word. Try again')
+            word_lowercase.find(guess_lowercase)
+
+            if word_lowercase.find(guess_lowercase) >= 0:
+                print(f'Good guess! {guess_lowercase} is in the word.')
+                letter_position = word_lowercase.index(guess_lowercase)
+                word_guessed[letter_position] = guess_lowercase
+                print(word_guessed)
+            else:
+                print(f'Sorry, {guess_lowercase} is not in the word. Try again.')
 #%%
 # Initialising the following attributes
 # word & word_list
@@ -91,36 +96,9 @@ word = random.choice(word_list)
 print(word)
 #%%
 ask_for_input(word)
-#%%
-# word_guessed
-# A list of the word with _ for each letter not yet guessed.
-def create_word_guessed():
-    word_guessed = [ ]
-
-    for letter in word:
-        word_guessed == word_guessed.append('_')
-    print(word_guessed)
-
-    # get guess value using ask_for_input function
-
-# Now need to replace _ with the letter guessed if it is in the word
-    while True: 
-        # guess = input('Please enter a single letter here: ')
-
-        ask_for_input(word)
-
-        for letter in word:
-            if guess_lowercase == letter:
-                letter_position = word.index(guess_lowercase)
-                print(letter_position)
-                word_guessed[letter_position] = guess_lowercase
-            else: 
-                print(f'{guess_lowercase} is not in the word. Try again')
 # %%
 # num_letters
 # The number of unique letters in the word that have not been guessed yet.
 # %%
 create_word_guessed()
-# %%
-print(word)
 # %%
